271837d77988dc6e7509cf38291b324e10cc9c15
juanda645 <juanda645@hotmail.com>
2015-04-28 10:31:53 -0500

    hola3

--- jiraJenkinsIntegration/src/main/java/com/jira/jenkins/jiraJenkinsIntegration/App.java
+++ jiraJenkinsIntegration/src/main/java/com/jira/jenkins/jiraJenkinsIntegration/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello JIRA and Jenkins" );
+        System.out.println( "Hello JIRA and Jenkins3" );
     }
 }

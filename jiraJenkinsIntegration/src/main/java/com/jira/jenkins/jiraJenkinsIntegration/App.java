993ab391b774ccf093fd4b97a5c6cb9d505e5c04
juanda645 <juanda645@hotmail.com>
2015-04-28 10:18:15 -0500

    Prube jira y jenkins

---
+++ jiraJenkinsIntegration/src/main/java/com/jira/jenkins/jiraJenkinsIntegration/App.java
@@ -0,0 +1,13 @@
+package com.jira.jenkins.jiraJenkinsIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
